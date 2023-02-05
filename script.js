5ccb691293f352f5ebceb5bcef6477aa551f4027
Valentin Blehaut <valentinblehaut@vb-mba.local>
2023-02-05 14:50:33 -0800

    First draft all files

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const font = document.main;
 
-const changeToBlue = (font) => {
-    font.style.color = blue;
+//alert("Beep boop");
+
+//alert to say something is happenning when button is clicked
+const mystButton = document.getElementById("mysteryButton");
+const somethingIsHappening = () => {
+    alert("Something is happening!");
 }
 
-footer.addEventListener('click', changeToBlue);
+//change the background to pink!
+const allContent = document.getElementById("content");
+const changeToPink = () => {
+    allContent.style.backgroundColor = "pink";
+    mystButton.style.backgroundColor = "skyblue";
+};
+
+
+mystButton.addEventListener("click", somethingIsHappening);
+mystButton.addEventListener("click", changeToPink);
+
